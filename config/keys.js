7cfb03c7ac2a06c587febce662ada5ebb6a1eaae
Sherman Tanuwidjaja <shermantanuwidjaja@gmail.com>
2018-11-16 16:42:36 +0800

    Set up JWT and passport authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://sherman:Pazzword123!@ds039321.mlab.com:39321/devconnector'
+    'mongodb://sherman:Pazzword123!@ds039321.mlab.com:39321/devconnector',
+  secretOrKey: 'secret'
 };
